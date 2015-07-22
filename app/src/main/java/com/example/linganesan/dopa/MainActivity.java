13637030bf4926c4c9ddb33013d29fd38df7c064
Linganesan <linganesan.12@cse.mrt.ac.lk>
2015-07-22 23:21:00 +0530

    more commits

--- app/src/main/java/com/example/linganesan/dopa/MainActivity.java
+++ app/src/main/java/com/example/linganesan/dopa/MainActivity.java
@@ -4,14 +4,72 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.content.SharedPreferences;
+import android.content.DialogInterface;
+import android.app.AlertDialog;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View.OnClickListener;
+import android.view.View;
 
 public class MainActivity extends ActionBarActivity {
 
+    public static final String PREFS_NAME = "MyPrefsFile";
+
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        boolean dialogShown = settings.getBoolean("dialogShown", false);
+
+        if (!dialogShown) {
+            // AlertDialog code here
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                    this);
+
+            // set title
+            alertDialogBuilder.setTitle("Hello! MemoryLord");
+
+            // set dialog message
+            alertDialogBuilder
+                    .setMessage("First Create a MindPalace or use the default one")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    // if this button is clicked, just close
+                                    // the dialog box and do nothing
+                                    dialog.cancel();
+                                }
+                            }
+                   );
+
+            // create alert dialog
+            AlertDialog alertDialog = alertDialogBuilder.create();
+
+            // show it
+            alertDialog.show();
+
+
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putBoolean("dialogShown", true);
+            editor.commit();
+        }
+
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this,
+                        mindpalace.class);
+                startActivity(myIntent);
+            }
+        });
+
     }
 
     @Override
